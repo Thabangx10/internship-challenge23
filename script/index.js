824226829f02cdac8a7db21e1e23213b2b422ccf
Thabangx10 <thabangx10@gmail.com>
2024-01-12 11:10:56 +0200

    check

--- script/index.js
+++ script/index.js
@@ -1,21 +1,34 @@
-$(document).ready(function () {
-  const carousel = $(".carousel");
-  const slides = $(".slide");
-  let currentSlide = 0;
+$(document).ready(function(){
+    $('.next').on('click', function(){
+    var currentImg = $('.active');
+    var nextImg = currentImg.next();
 
-  $(".next").click(function () {
-      currentSlide = (currentSlide + 1) % slides.length;
-      updateCarousel();
-  });
+    if(nextImg.length){
+        currentImg.removeClass().css('z-index', -10);
+        nextImg.addClass('active').css('z-index', 10);
+    }
 
-  $(".prev").click(function () {
-      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-      updateCarousel();
-  });
+    });
 
-  function updateCarousel() {
-      const slideWidth = slides.eq(0).width();
-      const newPosition = -currentSlide * slideWidth;
-      carousel.css("transform", `translateX(${newPosition}px)`);
-  }
+    $('.prev').on('click', function(){
+        var currentImg = $('.active');
+        var prevImg = currentImg.prev();
+    
+        if(prevImg.length){
+            currentImg.removeClass().css('z-index', -10);
+            prevImg.addClass('active').css('z-index', 10);
+        }
+    
+        });
+
+        $('.next').on('click', function(){
+            var currentSVG = $('.activeSVG');
+            var nextSVG = currentSVG.next();
+        
+            if(nextSVG.length){
+                currentSVG.removeClass();
+                nextSVG.addClass('activeSVG');
+            }
+        
+            });
 });
